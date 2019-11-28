8ebc0c0ba7ddb4b0e0c17179254f97288fa0652e
Marco Picchillo <m.picchillo@yahoo.com>
2019-11-28 15:35:28 +0000

    Completed

--- BinarySearch/Program.cs
+++ BinarySearch/Program.cs
@@ -1,9 +1,5 @@
 ﻿//Marco Picchillo - Sorting Array and BinarySearch
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BinarySearch
 {
